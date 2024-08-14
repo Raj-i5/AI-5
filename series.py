b661f7a717cee1a96494023ce0d671cf4143c2a4
Raj-i5 <raj2.0.ai.ml@gmail.com>
2024-08-14 11:30:56 +0530

    https://github.com/Raj-i5/AI-6.git

--- series.py
+++ series.py
@@ -1,13 +1,15 @@
 import pandas as pd
-from functools import reduce
-data = {
-'Numbers': [1, 2, 3, 4, 5],
-'Letters': ['A', 'B', 'C', 'D', 'E']
-}
-df = pd.DataFrame(data)
-sq=df['Numbers'].map(lambda x: x**2)
-ev=list(filter(lambda x: x % 2 == 0, df['Numbers']))
-po = reduce(lambda x, y: x * y, df['Numbers'])
-print("Dataframe:\n",df)
-print("\nMap for Squaring:\n",sq)
-print("\nReduce for product:\n", po)
+a = pd.Series([10, 20, 30, 40, 50])
+b = pd.Series([40, 50, 60, 70, 80])
+print("Series A:")
+print(a)
+print("\nSeries B:")
+print(b)
+non_com = a[~a.isin(b)].tolist() + b[~b.isin(a)].tolist()
+print("Items not common to both Series:")
+print(non_com)
+print("\nSmallest element in Series A:\n", a.min())
+print("\nLargest element in Series A:\n",a.max())
+print("\nSum of Series B:\n", b.sum())
+print("\nAverage of Series A:\n",a.mean())
+print("\nMedian of Series B:\n", a.median())
